631aa331fd76711b2e0c779b70d5ef86975420bc
Khalid <khalidkamil@icloud.com>
2021-01-07 23:29:08 +0000

    Initial Commit

---
+++ Project16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Khalid Kamil on 1/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
